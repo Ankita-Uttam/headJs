a5344f8ba1af4dde1c9a836f77332196140320fa
Ankita.uttam <ankita.uttam@IN-Ankitauttam.local>
2019-05-21 17:43:25 +0530

    added mocha path to test using describe and added failing behavorial test to headTest

--- headTest.js
+++ headTest.js
@@ -1,9 +1,15 @@
 const assert = require('assert');
-const describe = require('describe()');
 const head = require('./head');
 
-describe('test something', () => {
-    it ('test empty string', ()  => {
-        assert.deepStrictEqual(head.executeParsedCommand({}), '');
+describe('test head', () => {
+    it ('test default case', ()  => {
+        assert.deepStrictEqual(head.executeParsedCommand({
+            files: ['files/file1.txt'],
+            option: {
+                type: 'line',
+                count: 10,
+                illegalCount: null
+            }
+        }), '1\n2\n3\n4\n5\n6\n7\n8\n9\n10');
     });
 });
