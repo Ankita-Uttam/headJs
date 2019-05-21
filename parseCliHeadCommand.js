c038194a5cedc6571330f4cf59ce769c2c80769d
Ankita.uttam <ankita.uttam@IN-Ankitauttam.local>
2019-05-21 15:03:05 +0530

    replacing magic numbers and string literals with constants

--- parseCliHeadCommand.js
+++ parseCliHeadCommand.js
@@ -1,4 +1,10 @@
-module.exports.getParsedObject = (arguments) => {
+const DEFAULT_COUNT = 10;
+const BYTE_IDENTIFIER = '-c';
+const LINE_IDENTIFIER = '-n';
+const LINE = 'line';
+const BYTE = 'byte';
+
+getParsedObject = (arguments) => {
     const parsedObject = {
         files: [],
         option: {
@@ -18,11 +24,11 @@ function parseRemainingArguments(parsedObject , arguments) {
     let countArgument = null; // TODO - wrong scope
     let fileStartIndex = 0;
     if (parsedObject.option.type) {
-        if (arguments[0].endsWith("-n") || arguments[0].endsWith("-c")) { // TODO - long conditions. Give them names.
+        if (arguments[0].endsWith(LINE_IDENTIFIER) || arguments[0].endsWith(BYTE_IDENTIFIER)) { // TODO - long conditions. Give them names.
             countArgument = arguments[1];
             parsedObject.option.count = Number(countArgument);
             fileStartIndex = 2;
-        } else if (parsedObject.option.type === 'line' && !arguments[0].startsWith("-n")) {
+        } else if (parsedObject.option.type === LINE && !arguments[0].startsWith(LINE_IDENTIFIER)) {
             countArgument = arguments[0].substring(1, arguments[0].length);
             parsedObject.option.count = Number(countArgument);
             fileStartIndex = 1;
@@ -36,8 +42,8 @@ function parseRemainingArguments(parsedObject , arguments) {
             parsedObject.option.illegalCount = countArgument;
         }
     } else {
-        parsedObject.option.type = 'line';
-        parsedObject.option.count = 10; // TODO - magic literal
+        parsedObject.option.type = LINE;
+        parsedObject.option.count = DEFAULT_COUNT;
     }
 
     parsedObject.files = arguments.slice(fileStartIndex);
@@ -46,12 +52,14 @@ function parseRemainingArguments(parsedObject , arguments) {
 
 function getParsedOptionType(argument) {
     let type = null;
-    if (argument.startsWith("-n")) {
-        type = 'line';
-    } else if (argument.startsWith("-c")) {
-        type = 'byte';
+    if (argument.startsWith(LINE_IDENTIFIER)) {
+        type = LINE;
+    } else if (argument.startsWith(BYTE_IDENTIFIER)) {
+        type = BYTE;
     } else if (argument.startsWith("-") && Number(arguments[0].substring(1, 2))) {
-        type = 'line';
+        type = LINE;
     }
     return type;
 }
+
+module.exports = { getParsedObject, BYTE, LINE };
